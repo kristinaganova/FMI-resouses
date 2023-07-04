3b24199057e05a89f02a5a60abf85f50532de105
Kristina Ganova <115446275+kristinaganova@users.noreply.github.com>
2023-07-04 08:45:33 +0300

    Update StringPiece.h

--- OOP/Homework 2/String Creater Piece By Piece/StringPiece.h
+++ OOP/Homework 2/String Creater Piece By Piece/StringPiece.h
@@ -4,30 +4,31 @@ static const short MAX_PIECE_LEN = 16;
 class StringPiece
 {
 private:
-	const char* _begin;
-	const char* _end;
+	char _str[MAX_PIECE_LEN + 1]; 
+	unsigned int _start; 
+	unsigned int _end; 
 public:
-	StringPiece();
-	StringPiece(const char* begin, const char* end);
-	explicit StringPiece(const char* str);
-	~StringPiece() = default;
+    StringPiece();
+    StringPiece(const char* str);
+    StringPiece(const char* str, unsigned int start, unsigned int end);
+    ~StringPiece() = default;
 
-	const char* c_str() const;
-	const char* get() const;
-	void set(const char* str);
-	size_t length() const;
-	void append(const char* str, size_t len);
-	void appendInTheBeggining(const char* str, size_t  len);
-	void print() const;
+    const char* c_str() const;
+    const char* get() const;
+    void set(const char* str);
+    size_t length() const;
+    void append(const char* str, unsigned int len);
+    void appendInTheBeginning(const char* str, unsigned int len);
+    void print() const;
 
-	const char* toString() const;
+    const char* toString() const;
 
-	StringPiece& operator<<(const char* str);
-	StringPiece& operator<<(size_t num);
-	StringPiece& operator>>(const char* str);
-	StringPiece& operator>>(size_t num);
+    StringPiece& operator<<(const char* str);
+    StringPiece& operator<<(unsigned int num);
+    StringPiece& operator>>(const char* str);
+    StringPiece& operator>>(unsigned int num);
 
-	void removeFirstKChars(size_t k);
-	void removeLastKChars(size_t k);
-	void changeCharByIndex(int index, char c);
+    void removeFirstKChars(unsigned int k);
+    void removeLastKChars(unsigned int k);
+    void changeCharByIndex(int index, char c);
 };
